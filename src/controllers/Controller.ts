eb953c586fcd35d43758a527e41c0a367c25e190
yobeldc <103274795+yobeldc@users.noreply.github.com>
2023-11-16 00:32:25 +0700

    Update Controller.ts

--- src/controllers/Controller.ts
+++ src/controllers/Controller.ts
@@ -4,16 +4,22 @@ import noMiddleware from "../middlewares/noMiddleware";
 import { Usecase } from "../usecases/Usecase";
 import { PostController } from "./PostController";
 import { UserController } from "./UserController";
+import { MenfessController } from "./MenfessController";
+import { SubscriptionController } from "./SubscriptionController";
 
 export class Controller implements IController {
     private router: Router;
     private userController: UserController;
     private postController: PostController;
+    private menfessController: MenfessController;
+    private subscriptionController: SubscriptionController;
 
     constructor(usecase: Usecase) {
         this.router = Router();
         this.userController = new UserController(usecase);
         this.postController = new PostController(usecase);
+        this.menfessController = new MenfessController(usecase);
+        this.subscriptionController = new SubscriptionController(usecase);
         this.initalizeRouter();
     }
 
@@ -28,6 +34,16 @@ export class Controller implements IController {
             noMiddleware,
             this.postController.controllerRouter()
         );
+        this.router.use(
+            "/menfess",
+            noMiddleware,
+            this.menfessController.controllerRouter()
+        );
+        this.router.use(
+            "/subscription",
+            noMiddleware,
+            this.subscriptionController.controllerRouter()
+        )
     }
 
     public controllerRouter = (): Router => {
