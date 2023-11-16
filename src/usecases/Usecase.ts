54ae4ccf33d4f8a80629671a1e73f7b8a2a3b9cc
yobeldc <103274795+yobeldc@users.noreply.github.com>
2023-11-16 11:46:28 +0700

    Update Usecase.ts

--- src/usecases/Usecase.ts
+++ src/usecases/Usecase.ts
@@ -4,18 +4,21 @@ import { UserUsecase } from "./UserUsecase";
 import { Client } from "../clients/Client";
 import { MenfessUseCase } from "./MenfessUsecase";
 import { SubscriptionUsecase } from "./SubscriptionUsecase";
+import { StudioUseCase } from "./StudioUsecase";
 
 export class Usecase {
     private userUsecase: UserUsecase;
     private postUsecase: PostUsecase;
     private menfessUsecase: MenfessUseCase;
     private subscriptionUsecase: SubscriptionUsecase;
+    private studioUsecase: StudioUseCase;
 
     constructor(repo: PrismaClient, client: Client) {
         this.userUsecase = new UserUsecase(repo);
         this.postUsecase = new PostUsecase(repo);
         this.menfessUsecase = new MenfessUseCase(repo);
         this.subscriptionUsecase = new SubscriptionUsecase(repo);
+        this.studioUsecase = new StudioUseCase(repo);
     }
 
     get user() {
@@ -32,5 +35,9 @@ export class Usecase {
 
     get subscription() {
         return this.subscriptionUsecase;
+    }
+
+    get studio() {
+        return this.studioUsecase;
     }
 }
